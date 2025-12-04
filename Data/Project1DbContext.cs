9b63215f2b2b45e0935ede85c387042d41480788
teju666 <106806094+teju666@users.noreply.github.com>
2025-12-04 10:32:01 +0000

    rrr

--- Data/Project1DbContext.cs
+++ Data/Project1DbContext.cs
@@ -2,10 +2,15 @@ using Microsoft.EntityFrameworkCore;
 using Project1.Models.Entities;
 
 namespace Project1.Data
-public class Project1DbContext : DbContext
 {
-    public Project1DbContext(DbContextOptions<Project1DbContext> options) : base(options) { }
+    public class Project1DbContext : DbContext
+    {
+         public Project1DbContext(DbContextOptions<Project1DbContext> options) : base(options)
+        {
+        
+         }
 
     // DbSet for your Project entity
-    public DbSet<Project> Projects { get; set; }
+        public DbSet<Project> Projects { get; set; }
+    }
 }
--- obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa20abaa055d1e8034fe13120b64eaab14d679e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f2b07d72e94a620e3653825ec1a7147bf1d537f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
